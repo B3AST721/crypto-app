25a9298b8bd6d5d3b34fa7b4e3de02f82c4c6e63
Hansel Warrior <jaheelwarr@gmail.com>
2022-07-02 09:07:29 -0600

    fetched data from the api

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+
+  const [coins, setCoins] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false")
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      setCoins(data);
+    })
+  }, []);
+
   return (
     <div className="App">
-      
+      <ul>
+        {coins.map((coin) => (
+          <li key={coin.id}>{coin.name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
