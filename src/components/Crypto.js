3bc1bc586d08d0eb9d5c8dc26ec244d8e4ea0899
Hansel Warrior <jaheelwarr@gmail.com>
2022-07-02 12:02:57 -0600

    added styling

--- src/components/Crypto.js
+++ src/components/Crypto.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import '/home/jaheel/crypto-app/src/Crypto.css';
 
 function Crypto({ name, image, symbol, price, volume, priceChange, marketcap }) {
   return (
