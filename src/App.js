0b1ca60bb7d7abc1aa56e7c783c24b7a7530e713
Hansel Warrior <jaheelwarr@gmail.com>
2022-07-02 09:58:29 -0600

    added seach bar

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Crypto from './components/Crypto';
 function App() {
 
   const [coins, setCoins] = useState([]);
+  const [search, setSearch] = useState("");
 
   useEffect(() => {
     fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false")
@@ -16,14 +17,16 @@ function App() {
     })
   }, []);
 
-  console.log(coins.name)
+  const handleSearch = (e) => {
+    setSearch(e.target.value);
+  }
 
   return (
     <div className="App">
       <div className='search'>
         <h1 className='text'>Search a currency</h1>
         <form>
-          <input type='text' placeholder='Search' className='input' />
+          <input onChange={handleSearch} type='text' placeholder='Search' className='input' />
         </form>
       </div>
     </div>
