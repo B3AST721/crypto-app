5693c8b776db23c51930c4bbe7ba84fd4599b79b
Hansel Warrior <jaheelwarr@gmail.com>
2022-07-04 08:02:06 -0600

    used styled components instead of css

--- src/Crypto.js
+++ src/Crypto.js
@@ -1,29 +1,96 @@
 import React from 'react';
-import '/home/jaheel/crypto-app/src/Crypto.css';
+import styled from 'styled-components';
 
 function Crypto({ name, image, symbol, price, volume, priceChange, marketcap }) {
+
+    const CoinContainer = styled.div`
+        display: flex;
+        justify-content: center;
+    `
+
+    const CoinRow = styled.div`
+        display: flex;
+        flex-direction: row;
+        justify-items: start;
+        align-items: center;
+        height: 80px;
+        border-bottom: 1px solid #d7d7d7;
+        width: 900px;
+    `
+
+    const Coin = styled.div`
+        display: flex;
+        align-items: center;
+        padding-right: 24px;
+        min-width: 300px;
+    `
+
+    const Img = styled.img`
+        height: 30px;
+        width: 30px;
+        margin-right: 10px;
+    `
+
+    const H1 = styled.h1`
+        font-size: 16px;
+        width: 130px;
+    `
+
+    const CoinSymbol = styled.p`
+        text-transform: uppercase;
+    `
+
+    const CoinData = styled.div`
+        display: flex;
+        text-align: center;
+        justify-content: space-between;
+        width: 100%;
+    `
+
+    const CoinPrice = styled.p`
+        width: 110px;
+    `
+
+    const CoinVolume = styled.p`
+        width: 155px;
+    `
+
+    const RedPercent = styled.p`
+        width: 100%;
+        color: #f00606;
+    `
+
+    const GreenPercent = styled.p`
+        width: 100%;
+        #11d811;
+    `
+
+    const CoinMarketcap = styled.p`
+        width: 230px;
+    `
+
   return (
-    <div className='coin-container'>
-        <div className='coin-row'>
-            <div className='coin'>
-                <img src={image} alt='crypto' />
-                <h1>{name}</h1>
-                <p className='coin-symbol'>{symbol}</p>
-            </div>
-            <div className='coin-data'>
-                <p className='coin-price'>{price}</p>
-                <p className='coin-volume'>{volume.toLocaleString()}</p>
+    <CoinContainer>
+        <CoinRow>
+            <Coin>
+                <Img src={image} alt='crypto' />
+                <H1>{name}</H1>
+                <CoinSymbol>{symbol}</CoinSymbol>
+            </Coin>
+            <CoinData>
+                <CoinPrice>{price}</CoinPrice>
+                <CoinVolume>{volume.toLocaleString()}</CoinVolume>
                 {priceChange < 0 ? (
-                    <p className='coin-percent red'>{priceChange.toFixed(2)}%</p>
+                    <RedPercent>{priceChange.toFixed(2)}%</RedPercent>
                 ) : (
-                    <p className='coin-percent green'>{priceChange.toFixed(2)}%</p>
+                    <GreenPercent>{priceChange.toFixed(2)}%</GreenPercent>
                 )}
-                <p className='coin-marketcap'>
+                <CoinMarketcap>
                     Mkt Cap: {marketcap.toLocaleString()}
-                </p>
-            </div>
-        </div>
-    </div>
+                </CoinMarketcap>
+            </CoinData>
+        </CoinRow>
+    </CoinContainer>
   )
 }
 
